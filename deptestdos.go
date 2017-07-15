7cf42ce710d033848c6b709b8cf61bc1993055f1
Brendan <brendan@getharvest.com>
2017-07-15 14:54:36 -0700

    Add imports

--- deptestdos.go
+++ deptestdos.go
@@ -1,6 +1,12 @@
 package deptestdos
 
-import "github.com/brendan-munro/deptest"
+import (
+  "github.com/brendan-munro/deptest"
+  "github.com/brendan-munro/deptest/floats"
+  "github.com/brendan-munro/deptest/strings"
+)
 
 type other deptest.Foo
+type another floats.CoolFloat
+type yetAnother strings.CoolString
 type Bar int
